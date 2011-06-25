a269f4ddd9bf1536d085fd689351554d3a165405
John Hinnegan <john.hinnegan@gmail.com>
2011-06-25 13:49:58 -0700

    initial rails gen

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleAuthlogic::Application.config.secret_token = 'c3118842c6b68b80eebcb2335a5c20def79cf2c8f3ac17a0b9e95f759b1bba59c9d0b80b608f6df7563a788854d80b30ca5bcc8144f38f8800a01a4e83a06cfc'
