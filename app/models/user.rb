9523c6eda40614b05de44baeb00ac16bc28bd266
John Hinnegan <john.hinnegan@gmail.com>
2011-06-25 17:36:32 -0700

    ignore some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@
 #
 
 class User < ActiveRecord::Base
-  acts_as_authentic 
+  acts_as_authentic do |c|
+    c.disable_perishable_token_maintenance = true
+    c.validate_login_field = false 
+    c.validate_email_field = false
+    c.validate_password_field = false 
+  end 
 
 end
